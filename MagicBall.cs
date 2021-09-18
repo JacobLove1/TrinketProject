d4170e6a7ebd3427456ad4b83dc63679fc9bf8a0
JacobLove1 <82552528+JacobLove1@users.noreply.github.com>
2021-09-18 15:05:27 -0500

    Add files via upload

---
+++ MagicBall.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using TMPro;
+using UnityEngine.Events;
+
+
+public class MagicBall : MonoBehaviour
+{
+    string[] phrases = new string[] { "It is Certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes.", "Most likely", "Outlook good", "Yes"
+,"Signs point to yes"
+,"Reply hazy, try again","Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again"
+,"Don't count on it" ,"My reply is no", "My sources say no" ,"Outlook not so good" ,"Very doubtful"};
+    public UnityEvent ButtonEvents;
+    public GameObject phraseBox;
+    public GameObject knickknack_base;
+    public Button myButton1;
+    public bool isTurn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        isTurn = false;
+    }
+
+
+
+
+    // Update is called once per frame
+    // Update is called once per frame
+    void Update()
+    {
+        if ((knickknack_base.transform.up.y < 0f) && (isTurn == false))
+        {
+
+            int phrase = Random.Range(0, phrases.Length);
+            phraseBox.GetComponent<TextMeshPro>().text = phrases[phrase];
+            Button b = myButton1.GetComponent<Button>();
+            b.onClick.Invoke();
+            isTurn = true;
+        }
+        if (knickknack_base.transform.up.y > 0f)
+        {
+
+            isTurn = false;
+        }
+    }
+
+}
+
---
+++ RocketSayings.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using TMPro;
+using UnityEngine.Events;
+
+public class RocketSayings : MonoBehaviour
+{
+    string[] phrases = new string[] { "Seems Like It", "Sure, Whatever.", "Yup!", "Why Not?", "Who could stop you?", "You have known the answer all along", "The Friends We Made Along The Way!", "I feel unqualified to address that", "Who knows? Tim does, but who wants to ask him?"
+,"I'm just a box!"
+,"I wasn't really listening","Ask your mother, she knows things?","I'd love to answer that, but I'd rather do anything else", "42", "Eh?" ,"I think not!" ,"That would displease the gods.", "The suggestion is absurd!" ,"Nah!" ,"I love you, but just, no."};
+    public UnityEvent ButtonEvents;
+    public GameObject phraseBox;
+    public GameObject knickknack_base;
+    public Button myButton;
+    public bool isTurn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        isTurn = false;
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((knickknack_base.transform.up.y < 0f) && (isTurn == false))
+        {
+
+            int phrase = Random.Range(0, phrases.Length);
+            phraseBox.GetComponent<TextMeshPro>().text = phrases[phrase];
+            Button btn = myButton.GetComponent<Button>();
+            btn.onClick.Invoke();
+            isTurn = true;
+        }
+        if (knickknack_base.transform.up.y > 0f)
+        {
+
+            isTurn = false;
+        }
+    }
+
+}
